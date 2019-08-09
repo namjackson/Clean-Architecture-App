6574c37c75c783a60690e1949e0002a9aa0f0ac0
namjackson <skawo32167@naver.com>
2019-08-10 01:41:12 +0900

    Base Activity,Fragment 생성

---
+++ app/src/main/java/com/namjackson/arch/sample/view/base/BaseActivity.kt
@@ -0,0 +1,19 @@
+package com.namjackson.arch.sample.view.base
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+
+abstract class BaseActivity<B : ViewDataBinding>(
+    private val layoutId: Int
+) : AppCompatActivity() {
+    protected lateinit var binding: B
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, layoutId)
+        binding.setLifecycleOwner { this.lifecycle }
+    }
+
+}
